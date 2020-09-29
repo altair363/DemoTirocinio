766fe46e5d0ab21d231259770f376c55a7a7c1f3
PeppeT <nestadj123@gmail.com>
2020-09-29 18:43:25 +0200

    clean the code

--- src/index.js
+++ src/index.js
@@ -77,14 +77,4 @@ const WebcamStreamCapture = () => {
 
 ReactDOM.render(<WebcamStreamCapture />, document.getElementById("root"));
 
-/*ReactDOM.render(
-  <React.StrictMode>
-    <WebcamComponent />
-  </React.StrictMode>,
-  document.getElementById('root')
-);*/
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
